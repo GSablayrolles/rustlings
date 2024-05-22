bdaf0c76af6165261eca232840219f536e628fe5
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2024-05-22 20:22:05 +0200

    Quiz1 done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i32) -> i32{
+    let cost: i32 = if number <= 40 {2} else {1};
 
+    cost*number
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
