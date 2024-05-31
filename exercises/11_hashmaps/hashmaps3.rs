8094f349b02844a5ad034882db4b7e48af51b28e
Guillaume Sablayrolles <g.sablayrolles@proton.me>
2024-05-31 13:47:11 +0200

    HashMap done

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -15,7 +15,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,7 +39,54 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
-    }
+        if ! scores.contains_key(&team_1_name) {
+            let team = Team{
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            };
+            scores.insert(team_1_name, team);
+        } else {
+            let old_score = scores.get(&team_1_name);
+            match old_score {
+                Some(t) => {
+                    let scored = t.goals_scored;
+                    let conceded = t.goals_conceded;
+
+                    let team = Team{
+                        goals_scored: team_1_score + scored,
+                        goals_conceded: team_2_score + conceded
+                    };
+        
+                    scores.insert(team_1_name, team);
+                },
+                None => (),
+            }
+        }
+        
+        if ! scores.contains_key(&team_2_name) {
+            let team = Team{
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score
+            };
+            scores.insert(team_2_name, team);
+        } else {
+            let old_score = scores.get(&team_2_name);
+            match old_score {
+                Some(t) => {
+                    let scored = t.goals_scored;
+                    let conceded = t.goals_conceded;
+
+                    let team = Team{
+                        goals_scored: team_2_score + scored,
+                        goals_conceded: team_1_score + conceded
+                    };
+        
+                    scores.insert(team_2_name, team);
+                },
+                None => (),
+            }
+        }
+    } 
     scores
 }
 
